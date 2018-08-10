0bb7297951110678b0679e0bc3a882b2669b63de
zero <zero.griffin@gmail.com>
2018-08-10 08:16:19 +0200

    Regroup entities

--- Domain/Shared/Localized.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-class Localized {
-    class func string(key:String) -> String {
-        return NSLocalizedString(key, tableName:"Domain", bundle:Bundle(for:Localized.self), comment:String())
-    }
-    
-    private init() { }
-}

c730394a43facccf3db77607dc667fcb850548fc
zero <zero.griffin@gmail.com>
2018-08-20 09:30:55 +0200

    Add cards font to session

--- Source/Entities/Session.swift
+++ Source/Entities/Session.swift
@@ -1,24 +1,33 @@
 import Foundation
 
 public class Session:Codable {
+    public struct CardsFont {
+        public static let original:Int = 14
+        public static let min:Int = 8
+        public static let max:Int = 30
+    }
+    
     public private(set) var boards:[String:Board]
+    public var cardsFont:Int
+//    public private(set) var defaultColumns:Bool
     
     public required init(from decoder:Decoder) throws {
         let values:KeyedDecodingContainer<CodingKeys> = try decoder.container(keyedBy:CodingKeys.self)
-        let keys:[String] = try values.decode([String].self, forKey:CodingKeys.boards)
         self.boards = [:]
-        keys.forEach { (key:String) in
-            self.boards[key] = Board()
-        }
+        self.cardsFont = 0
+        self.decodeBoards(values:values)
+        self.decodeCardsFont(values:values)
     }
     
     public func encode(to encoder:Encoder) throws {
         var container:KeyedEncodingContainer<CodingKeys> = encoder.container(keyedBy:CodingKeys.self)
         try container.encode(Array(self.boards.keys), forKey:CodingKeys.boards)
+        try container.encode(self.cardsFont, forKey:CodingKeys.cardsFont)
     }
     
     init() {
         self.boards = [:]
+        self.cardsFont = CardsFont.original
     }
     
     func add(board:String) {
@@ -35,7 +44,25 @@ public class Session:Codable {
         self.boards.removeValue(forKey:board)
     }
     
+    private func decodeBoards(values:KeyedDecodingContainer<CodingKeys>) {
+        let keys:[String]
+        do { try keys = values.decode([String].self, forKey:CodingKeys.boards) } catch { return }
+        keys.forEach { (key:String) in
+            self.boards[key] = Board()
+        }
+    }
+    
+    private func decodeCardsFont(values:KeyedDecodingContainer<CodingKeys>) {
+        do {
+            try self.cardsFont = values.decode(Int.self, forKey:CodingKeys.cardsFont)
+        } catch {
+            self.cardsFont = CardsFont.original
+        }
+    }
+    
     private enum CodingKeys:CodingKey {
         case boards
+        case cardsFont
+        case defaultColumns
     }
 }

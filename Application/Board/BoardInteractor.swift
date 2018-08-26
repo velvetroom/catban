add9e3691e099c3505445a90128f3c5efe086eb5
zero <zero.griffin@gmail.com>
2018-08-26 13:58:24 +0200

    Remove clutter

--- Application/Board/BoardInteractor.swift
+++ Application/Board/BoardInteractor.swift
@@ -6,16 +6,16 @@ class BoardInteractor:Interactor, InfoInteractor {
     weak var delegate:InteractorDelegate?
     var board:Board!
     var identifier:String
-    var cardsFont:Int { get { return self.library.cardsFont } }
+    var cardsFont:Int { return library.cardsFont }
     var boardName:String { return board.text }
     var boardUrl:String { return library.url(identifier:identifier) }
     private let library:Library
     private let report:Report
     
     required init() {
-        self.library = Factory.makeLibrary()
-        self.report = Report()
-        self.identifier = String()
+        library = Factory.makeLibrary()
+        report = Report()
+        identifier = String()
     }
     
     func detach(card:Card, column:Column) {
@@ -28,80 +28,75 @@ class BoardInteractor:Interactor, InfoInteractor {
         } else {
             column.makeFirst(card:card)
         }
-        self.save()
+        save()
     }
     
     func share() {
-        let view:ShareView = ShareView(presenter:SharePresenter())
+        let view = ShareView(presenter:SharePresenter())
         view.presenter.interactor = self
-        Application.router.present(view, animated:true, completion:nil)
+        Application.router.present(view, animated:true)
     }
     
     func edit() {
-        let text:TextChange = TextChange()
+        let text = TextChange()
         text.title = NSLocalizedString("BoardInteractor.boardTitle", comment:String())
-        text.text = self.board.text
-        text.subject = self.board
-        self.edit(text:text, delete:DeleteBoard(), infoSource:nil)
+        text.text = board.text
+        text.subject = board
+        edit(text:text, delete:DeleteBoard(), infoSource:nil)
     }
     
     func info() {
-        let view:InfoView = InfoView(presenter:InfoPresenter<BoardInteractor>())
+        let view = InfoView(presenter:InfoPresenter<BoardInteractor>())
         view.presenter.interactor = self
-        view.presenter.source = Constants.infoBoard
-        Application.router.present(view, animated:true, completion:nil)
+        view.presenter.source = "InfoBoard"
+        Application.router.present(view, animated:true)
     }
     
     func newColumn() {
-        self.edit(text:TextCreateColumn(), delete:nil, infoSource:nil)
+        edit(text:TextCreateColumn(), delete:nil, infoSource:nil)
     }
     
     func editColumn(column:Column) {
-        let text:TextChange = TextChange()
+        let text = TextChange()
         text.title = NSLocalizedString("BoardInteractor.columnTitle", comment:String())
         text.text = column.text
         text.subject = column
-        let delete:DeleteColumn = DeleteColumn()
+        let delete = DeleteColumn()
         delete.column = column
-        self.edit(text:text, delete:delete, infoSource:nil)
+        edit(text:text, delete:delete, infoSource:nil)
     }
     
     func newCard(column:Column) {
-        let text:TextCreateCard = TextCreateCard()
+        let text = TextCreateCard()
         text.column = column
-        self.edit(text:text, delete:nil, infoSource:Constants.infoCard)
+        edit(text:text, delete:nil, infoSource:"InfoCard")
     }
     
     func editCard(column:Column, card:Card) {
-        let text:TextChange = TextChange()
+        let text = TextChange()
         text.title = column.text
         text.text = card.text
         text.subject = card
-        let delete:DeleteCard = DeleteCard()
+        let delete = DeleteCard()
         delete.column = column
         delete.card = card
-        self.edit(text:text, delete:delete, infoSource:Constants.infoCard)
+        edit(text:text, delete:delete, infoSource:"InfoCard")
     }
     
     func save() {
-        self.library.save(board:self.board)
+        library.save(board:board)
     }
     
     func makeStats() -> ReportStats {
-        return self.report.makeStats(board:self.board)
+        return report.makeStats(board:board)
     }
     
     private func edit(text:TextStrategy, delete:DeleteStrategy?, infoSource:String?) {
-        let view:EditView = EditView(presenter:EditPresenter())
+        let view = EditView(presenter:EditPresenter())
         view.presenter.strategyText = text
         view.presenter.strategyDelete = delete
         view.presenter.interactor = self
         view.presenter.infoSource = infoSource
         Application.router.pushViewController(view, animated:true)
     }
-}
-
-private struct Constants {
-    static let infoCard:String = "InfoCard"
-    static let infoBoard:String = "InfoBoard"
 }

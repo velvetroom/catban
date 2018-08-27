6d849d7c429fd687d24a6272217dd7508171f1b6
zero <zero.griffin@gmail.com>
2018-08-27 08:17:52 +0200

    Remove clutter

--- Application/Board/BoardButtonView.swift
+++ Application/Board/BoardButtonView.swift
@@ -4,33 +4,25 @@ class BoardButtonView:BoardItemView {
     weak var image:UIImageView!
     
     override func makeOutlets() {
-        super.makeOutlets()
-        
-        let image:UIImageView = UIImageView()
+        let image = UIImageView()
         image.isUserInteractionEnabled = false
         image.translatesAutoresizingMaskIntoConstraints = false
         image.clipsToBounds = true
-        image.contentMode = UIView.ContentMode.bottomLeft
+        image.contentMode = .bottomLeft
+        addSubview(image)
         self.image = image
-        self.addSubview(image)
-    }
-    
-    override func layoutOutlets() {
-        super.layoutOutlets()
         
-        self.image.topAnchor.constraint(equalTo:self.topAnchor).isActive = true
-        self.image.bottomAnchor.constraint(equalTo:self.bottomAnchor).isActive = true
-        self.image.leftAnchor.constraint(equalTo:self.leftAnchor).isActive = true
-        self.image.rightAnchor.constraint(equalTo:self.rightAnchor).isActive = true
+        image.topAnchor.constraint(equalTo:topAnchor).isActive = true
+        image.bottomAnchor.constraint(equalTo:bottomAnchor).isActive = true
+        image.leftAnchor.constraint(equalTo:leftAnchor).isActive = true
+        image.rightAnchor.constraint(equalTo:rightAnchor).isActive = true
     }
     
     override func showSelected() {
-        super.showSelected()
-        self.alpha = 0.15
+        alpha = 0.15
     }
     
     override func showDefault() {
-        super.showDefault()
-        self.alpha = 1.0
+        alpha = 1
     }
 }

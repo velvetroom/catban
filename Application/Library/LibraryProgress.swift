2bda1507bd049fee6ea93fc8003a4253a21520de
zero <zero.griffin@gmail.com>
2018-09-05 00:22:35 +0100

    Update today cell view

--- Application/Library/LibraryProgress.swift
+++ Application/Library/LibraryProgress.swift
@@ -18,14 +18,14 @@ class LibraryProgress:UIView {
         let pi_2 = CGFloat.pi / 2
         let end = (CGFloat.pi * 2 * value) - pi_2
         guard let context = UIGraphicsGetCurrentContext() else { return }
-        context.setFillColor(tintColor.withAlphaComponent(0.1).cgColor)
+        context.setFillColor(tintColor.withAlphaComponent(0.05).cgColor)
         context.addArc(center:center, radius:radius - 7, startAngle:0.0001, endAngle:0, clockwise:false)
         context.drawPath(using:.fill)
         context.setStrokeColor(tintColor.cgColor)
         context.setLineWidth(6)
         context.addArc(center:center, radius:radius, startAngle:-pi_2, endAngle:end, clockwise:false)
         context.drawPath(using:.stroke)
-        context.setStrokeColor(tintColor.withAlphaComponent(0.1).cgColor)
+        context.setStrokeColor(tintColor.withAlphaComponent(0.05).cgColor)
         context.setLineWidth(4)
         context.addArc(center:center, radius:radius, startAngle:end + 0.15, endAngle:-pi_2 - 0.15, clockwise:false)
         context.drawPath(using:.stroke)
--- Today/TodayCellView.swift
+++ Today/TodayCellView.swift
@@ -19,7 +19,7 @@ class TodayCellView:UIControl {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.isUserInteractionEnabled = false
-        label.font = .systemFont(ofSize:11, weight:.light)
+        label.font = .systemFont(ofSize:12, weight:.regular)
         label.textColor = .black
         label.textAlignment = .center
         label.text = item.name
@@ -30,13 +30,13 @@ class TodayCellView:UIControl {
         progress.value = CGFloat(item.progress)
         addSubview(progress)
         
-        label.bottomAnchor.constraint(equalTo:bottomAnchor, constant:-12).isActive = true
+        label.bottomAnchor.constraint(equalTo:bottomAnchor, constant:-10).isActive = true
         label.leftAnchor.constraint(equalTo:leftAnchor, constant:6).isActive = true
         label.rightAnchor.constraint(equalTo:rightAnchor, constant:-6).isActive = true
         
-        progress.topAnchor.constraint(equalTo:topAnchor, constant:15).isActive = true
-        progress.bottomAnchor.constraint(equalTo:bottomAnchor, constant:-30).isActive = true
-        progress.leftAnchor.constraint(equalTo:leftAnchor, constant:15).isActive = true
-        progress.rightAnchor.constraint(equalTo:rightAnchor, constant:-15).isActive = true
+        progress.topAnchor.constraint(equalTo:topAnchor, constant:8).isActive = true
+        progress.bottomAnchor.constraint(equalTo:bottomAnchor, constant:-26).isActive = true
+        progress.leftAnchor.constraint(equalTo:leftAnchor, constant:10).isActive = true
+        progress.rightAnchor.constraint(equalTo:rightAnchor, constant:-10).isActive = true
     }
 }

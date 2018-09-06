2b1abcfe3435e8e783a78d0b63a714c7f7ef46de
zero <zero.griffin@gmail.com>
2018-09-06 16:27:02 +0100

    Add tests for board migration

--- Tests/TestBoard.swift
+++ Tests/TestBoard.swift
@@ -32,4 +32,29 @@ class TestBoard:XCTestCase {
         XCTAssertEqual(2, board.columns[1].cards.count)
         XCTAssertEqual("hello world", board.columns[1].cards[0].content)
     }
+    
+    func testLoadLegacyBoard() {
+        guard
+            let data = try? JSONSerialization.data(withJSONObject:["text":"hello world", "syncstamp":1, "columns":[]]),
+            let board = try? JSONDecoder().decode(Board.self, from:data)
+        else { return XCTFail() }
+        XCTAssertEqual("hello world", board.name)
+    }
+    
+    func testSaveNewBoard() {
+        let date = Date()
+        let board = Board()
+        board.name = "hello world"
+        board.syncstamp = date
+        board.addColumn(text:"lorem ipsum")
+        board.addColumn(text:"column b")
+        guard
+            let data = try? JSONEncoder().encode(board),
+            let decoded = try? JSONDecoder().decode(Board.self, from:data)
+        else { return XCTFail() }
+        XCTAssertEqual("hello world", decoded.name)
+        XCTAssertEqual(date, decoded.syncstamp)
+        XCTAssertEqual("lorem ipsum", decoded.columns[0].name)
+        XCTAssertEqual("column b", decoded.columns[1].name)
+    }
 }

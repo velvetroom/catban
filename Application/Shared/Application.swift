e5d27c7ffb683b845dfafebf954ba58684d1fde8
vauxhall <zero.griffin@gmail.com>
2018-10-14 13:11:01 +0200

    Refactor application load quick actions

--- Application/Shared/Application.swift
+++ Application/Shared/Application.swift
@@ -16,16 +16,12 @@ import Firebase
     
     func application(_:UIApplication, performActionFor item:UIApplicationShortcutItem,
                      completionHandler:@escaping(Bool) -> Void) {
-        switch item.type {
-        case "catban.addBoard": Application.navigation.quickAdd()
-        case "catban.loadBoard": Application.navigation.quickScan()
-        default: break
-        }
+        shortcut(item:item)
     }
     
     func application(_:UIApplication, open url:URL, options:[UIApplication.OpenURLOptionsKey:Any] = [:]) -> Bool {
-        if let identifier = url.absoluteString.components(separatedBy:"catban:board=").last {
-            Application.navigation.quick(board:identifier)
+        if let board = try? boardFrom(url:url) {
+            Application.navigation.quick(board:board)
             return true
         }
         return false
@@ -48,20 +44,34 @@ import Firebase
         window!.rootViewController = Application.navigation
     }
     
+    private func shortcut(item:UIApplicationShortcutItem) {
+        switch item.type {
+        case "catban.addBoard": Application.navigation.quickAdd()
+        case "catban.loadBoard": Application.navigation.quickScan()
+        default: break
+        }
+    }
+    
     private func launch(options:[UIApplication.LaunchOptionsKey:Any]?) -> Bool {
         var needsLaunch = false
-        if let identifier = (options?[.url] as? URL)?.absoluteString.components(separatedBy:"catban:board=").last {
-            Application.navigation.launch(board:identifier)
+        if let url = options?[.url] as? URL,
+            let board = try? boardFrom(url:url) {
+            Application.navigation.launch(board:board)
         } else {
             Application.navigation.launchDefault()
-            if let shortcut = options?[.shortcutItem] as? UIApplicationShortcutItem {
-                switch shortcut.type {
-                case "catban.addBoard": Application.navigation.launchAdd()
-                case "catban.loadBoard": Application.navigation.launchScan()
-                default: break
-                }
+            if let item = options?[.shortcutItem] as? UIApplicationShortcutItem {
+                shortcut(item:item)
             } else { needsLaunch = true }
         }
         return needsLaunch
+    }
+    
+    private func boardFrom(url:URL) throws -> String {
+        let components = url.absoluteString.components(separatedBy:"catban:board=")
+        if components.count == 2,
+            !components[1].isEmpty {
+            return components[1]
+        }
+        throw CatbanError.invalidBoardUrl
     }
 }
